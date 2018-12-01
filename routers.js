2d1f41cd9f65578df1725c9dc604f41e00afcef8
surekha <techqaq@gmail.com>
2018-12-01 07:48:16 +0000

    commit

--- routers.js
+++ routers.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-const cors = require("cors");
 
 // Config
 const { routes } = require('./config.json');
@@ -10,13 +9,9 @@ const port = 80;
 
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
-  
+
+
 for (route of routes) {
     app.use(route.route,
         proxy({
