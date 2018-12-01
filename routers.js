6062b32fe7ebf10c184926a951c705afd9a31598
surekha <techqaq@gmail.com>
2018-12-01 06:54:59 +0000

    commit

--- routers.js
+++ routers.js
@@ -13,18 +13,10 @@ const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.options('*', cors()); 
-
-app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
     next();
-});
+  });
 
+  
 for (route of routes) {
     app.use(route.route,
         proxy({
